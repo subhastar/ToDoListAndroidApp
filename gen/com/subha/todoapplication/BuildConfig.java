7d802358654fcafe16fe3db619fb9312aa0d4c24
Subha Gollakota <subha.gollakota@gmail.com>
2013-09-19 20:56:31 -0700

    first commit

---
+++ gen/com/subha/todoapplication/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.subha.todoapplication;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/subha/todoapplication/ToDoActivity.java
@@ -0,0 +1,93 @@
+package com.subha.todoapplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class ToDoActivity extends Activity {
+	private ArrayList<String> todoItems;
+	private ArrayAdapter<String> todoAdapter;
+	private ListView lvItems;
+	private EditText etNewItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_do);
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        todoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+        lvItems.setAdapter(todoAdapter);
+        setupListViewListener();
+    }
+    
+    private void setupListViewListener() {
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> arg0, View item,
+					int pos, long id) {
+				todoItems.remove(pos);
+				todoAdapter.notifyDataSetChanged();
+				writeItems();
+				return true;
+			}
+    		
+    	});
+    }
+    
+    private void populateArrayItems() {
+    	todoItems = new ArrayList<String>();
+    	todoItems.add("item 1");
+    	todoItems.add("item 2");
+    	todoItems.add("item 3");
+    }
+    
+    public void onAddedItem(View v) {
+    	String itemText = etNewItem.getText().toString();
+    	todoAdapter.add(itemText);
+    	etNewItem.setText("");
+    	writeItems();
+    }
+    
+    private void readItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+    	} catch (IOException e) {
+    		todoItems = new ArrayList<String>();
+    	}
+    }
+    
+    private void writeItems() {
+    	File filesDir = getFilesDir();
+    	File todoFile = new File(filesDir, "todo.txt");
+    	try {
+    		FileUtils.writeLines(todoFile, todoItems);
+    	} catch (IOException e) {
+    		e.printStackTrace();
+    	}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.to_do, menu);
+        return true;
+    }
+    
+}
